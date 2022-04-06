f0c2884731cbe5e25df4e751870d49167a81687b
Usen <83946606+Usen-ita@users.noreply.github.com>
2022-04-06 09:50:42 +0100

    h

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useRef} from "react";
+import {FaBars, FaTimes} from "react-icons/fa";
 
-export default function Header() {
+export default function Header(){
+  const navRef = useRef();
+
+  const showNavbar = () =>{
+    navRef.current.classList.toggle("responsive_nav");
+  }
+
   return (
-    <header>
+    /*{<header>
       <div className='header-inner'>
         <div className='logo'>CHAIR.</div>
-        <nav>
+        <nav >
           <ul>
             <li>
               <a href='/'>discover</a>
@@ -25,6 +32,24 @@ export default function Header() {
           </ul>
         </nav>
       </div>
+    </header>}*/
+    <header>
+      <div className= 'logo'>CHAIR.</div>
+      <nav ref={navRef}>
+        <a href="/#">Home</a>
+        <a href="/#">products</a>
+        <a href="/#">solutions</a>
+        <a href="/#">reach</a>
+        <button className= 'btn'>
+          <a href="/#">order</a>
+        </button>
+        <button className = "nav-btn nav-close-btn"onClick={showNavbar}>
+      <FaTimes/>
+      </button>
+      </nav>
+      <button  className = "nav-btn" onClick={showNavbar}>
+      <FaBars/>
+      </button>
     </header>
   );
 }
